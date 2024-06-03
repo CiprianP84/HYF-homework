a567527c31d4fc1d382a8dfbfeb83050124c2424
CiprianP84 <ciprian.pirciu@gmail.com>
2024-06-03 16:39:25 +0200

    Age calculator added

---
+++ JavaScript/JavaScript1/Week1/script.js
@@ -0,0 +1,6 @@
+// Age-ify (A future age calculator)
+
+const yearOfBirth = 1984;
+const yearFuture = 2057;
+const age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} years old in ${yearFuture}.`);
