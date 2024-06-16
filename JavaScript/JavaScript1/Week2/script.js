463d6300dd27a1397753c74cea093b95af3d3fc5
CiprianP84 <ciprian.pirciu@gmail.com>
2024-06-16 10:29:01 +0200

    event application

--- JavaScript/JavaScript1/Week2/script.js
+++ JavaScript/JavaScript1/Week2/script.js
@@ -19,3 +19,17 @@ const fullname2 = getFullname("Emily", "Brown", true, "F");
 console.log(fullname1);
 console.log(fullname2);
 
+//Event application
+function getEventWeekday(daysFromToday) {
+  const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+  const today = new Date();
+  const todayDayIndex = today.getDay();
+  const eventDayIndex = (todayDayIndex + daysFromToday) % 7;
+  const eventDay = weekdays[eventDayIndex];
+  console.log(eventDay);
+  return eventDay;
+}
+// Testing the function
+console.log(getEventWeekday(9));
+console.log(getEventWeekday(2));
