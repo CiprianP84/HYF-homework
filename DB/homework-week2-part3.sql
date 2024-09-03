dea11f407ba4d182647e79ff78e5890e15c54aa5
CiprianP84 <ciprian.pirciu@gmail.com>
2024-09-03 20:43:05 +0200

    PART 3 - queries for lesson2-data DB

---
+++ DB/homework-week2-part3.sql
@@ -0,0 +1,29 @@
+-- 1. Get All Tasks Assigned to Users Whose Email Ends in @spotify.com
+SELECT t.*
+FROM task t
+JOIN user_task ut ON t.id = ut.task_id
+JOIN user u ON ut.user_id = u.id
+WHERE u.email LIKE '%@spotify.com';
+
+-- 2. Get All Tasks for 'Donald Duck' with Status 'Not started'
+SELECT t.*
+FROM task t
+JOIN user_task ut ON t.id = ut.task_id
+JOIN user u ON ut.user_id = u.id
+JOIN status s ON t.status_id = s.id
+WHERE u.name = 'Donald Duck'
+  AND s.name = 'Not started';
+
+-- 3. Get All Tasks for 'Maryrose Meadows' That Were Created in September
+SELECT t.*
+FROM task t
+JOIN user_task ut ON t.id = ut.task_id
+JOIN user u ON ut.user_id = u.id
+WHERE u.name = 'Maryrose Meadows'
+  AND MONTH(t.created) = 9;
+
+-- 4. Find How Many Tasks Were Created in Each Month
+SELECT MONTH(t.created) AS month, COUNT(*) AS task_count
+FROM task t
+GROUP BY MONTH(t.created);
+
